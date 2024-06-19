8fcb7e70a63b63880388400b8720ca398a9089d5
0xcrypto <6683856+0xcrypto@users.noreply.github.com>
2024-06-19 14:37:08 +0530

    added support for out of scope

---
+++ csv.go
@@ -0,0 +1,57 @@
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func hackerone_csv() {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.TrimLeadingSpace = true
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	for _, columns := range records {
+		if len(columns) > 1 {
+			identifier := columns[0]
+			assetType := columns[1]
+			eligible_for_submission := columns[4]
+
+			if eligible_for_submission != "true" {
+				assetType = "out_of_scope"
+			}
+
+			if identifier == "identifier" {
+				// skip the csv header
+				continue
+			}
+
+			file, err := os.OpenFile(strings.ToLower(assetType)+"_targets.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Println("Error opening file:", err)
+				return
+			}
+			defer file.Close()
+
+			_, err = file.WriteString(identifier + "\n")
+			if err != nil {
+				fmt.Println("Error appending to file:", err)
+				return
+			}
+		} else {
+			fmt.Println("Invalid input format")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
-	"fmt"
 	"os"
-	"strings"
+	"csv"
 )
 
 var inputFile string
@@ -21,45 +19,6 @@ func init() {
 
 func main() {
 
-	file, err := os.Open(inputFile)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
+	hackerone_csv(inputFile)
 
-	reader := csv.NewReader(file)
-	reader.TrimLeadingSpace = true
-
-	records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-
-	for _, columns := range records {
-		if len(columns) > 1 {
-			identifier := columns[0]
-			assetType := columns[1]
-
-			if identifier == "identifier" {
-				continue
-			}
-
-			file, err := os.OpenFile(strings.ToLower(assetType)+"_targets.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println("Error opening file:", err)
-				return
-			}
-			defer file.Close()
-
-			_, err = file.WriteString(identifier + "\n")
-			if err != nil {
-				fmt.Println("Error appending to file:", err)
-				return
-			}
-		} else {
-			fmt.Println("Invalid input format")
-		}
-	}
 }
